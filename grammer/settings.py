2f82f245d37e818c4b0d6b0b41a2a839afc1c009
Muia23 <wilfred.muema@gmail.com>
2020-08-07 15:57:51 +0300

    add base template and install bootstrap

--- grammer/settings.py
+++ grammer/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'user',
+    'django.contrib.staticfiles',    
 ]
 
 MIDDLEWARE = [
