b996d80f327f0afe271e7df66aa9c3fea38cfa87
Muia23 <wilfred.muema@gmail.com>
2020-08-09 18:41:21 +0300

    add template for creating posts

--- user/urls.py
+++ user/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     
     url(r'^$', views.index, name='home'),
+    url(r'^new/post$', views.new_post, name='new-post'),
     #url(r'^profile/(\d)/', views.profile, name='profile'),
 ]
 if settings.DEBUG:
